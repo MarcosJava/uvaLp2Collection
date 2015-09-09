60f85bab93cc1aeaafcf96264cff968ebf307f41
Marcos Felipe <mfelipesp@gmail.com>
2015-09-09 18:15:42 -0300

    acabando o managed do set

--- src/main/java/br/com/mrcsfelipe/collections/managed/SetMB.java
+++ src/main/java/br/com/mrcsfelipe/collections/managed/SetMB.java
@@ -1,12 +1,52 @@
 package br.com.mrcsfelipe.collections.managed;
 
+import java.util.HashSet;
 import java.util.Set;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Model;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 
 @Model
+@RequestScoped
 public class SetMB {
+	
 
-	private Set<String> listaDeSet;
+	private Set<String> palavras;
+	private String palavra;
+	
+	@Inject
+	private FacesContext facesContext;
+	
+	
+	
+	@PostConstruct
+	public void init(){
+		palavras = new HashSet<>();
+	}
+	
+	
+	public void registro() throws Exception{
+		this.palavras.add(this.palavra);
+	}
+
+
+	public String getPalavra() {
+		return palavra;
+	}
+
+
+	public void setPalavra(String palavra) {
+		this.palavra = palavra;
+	}
+
+
+	public Set<String> getPalavras() {
+		return palavras;
+	}
+	
+	
 	
 }
