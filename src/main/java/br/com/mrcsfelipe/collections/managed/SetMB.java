27911476b8b448f094f747f9ea7c86ca94fc6d55
Marcos Felipe <mfelipesp@gmail.com>
2015-09-09 17:58:17 -0300

    primeiro commits

---
+++ src/main/java/br/com/mrcsfelipe/collections/managed/SetMB.java
@@ -0,0 +1,12 @@
+package br.com.mrcsfelipe.collections.managed;
+
+import java.util.Set;
+
+import javax.enterprise.inject.Model;
+
+@Model
+public class SetMB {
+
+	private Set<String> listaDeSet;
+	
+}
