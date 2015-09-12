e90c4fa12bc1ab4469b9621f1c546717f70df220
Mark <mfelipesp@gmail.com>
2015-09-12 20:19:13 -0300

    vamos cambada

--- src/main/java/br/com/mrcsfelipe/collections/controller/MapController.java
+++ src/main/java/br/com/mrcsfelipe/collections/controller/MapController.java
@@ -1,7 +1,11 @@
 package br.com.mrcsfelipe.collections.controller;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
@@ -15,10 +19,16 @@ import br.com.mrcsfelipe.collections.model.Person;
 public class MapController {
 
 	private Map<String, String> mapList = new HashMap<>();
+	private List<Entry<String, String>> entries;
 	
 	@Named
 	private Person person;
 	
+	@PostConstruct
+	public void init(){
+		this.person = new Person();
+		this.entries = new ArrayList<>(this.mapList.entrySet());
+	}
 	
 	public void gerente(){
 		this.mapList.put("gerente", this.person.getName());
@@ -28,10 +38,19 @@ public class MapController {
 		this.mapList.put("coordenador", this.person.getName());
 	}
 
+	
+	public List<Entry<String, String>> getEntries() {
+		return entries;
+	}
+
 	public Map<String, String> getMapList() {
 		return mapList;
 	}
 
+	public String getGerentes(){
+		return this.mapList.get("gerente");
+	}
+	
 	public Person getPerson() {
 		return person;
 	}
